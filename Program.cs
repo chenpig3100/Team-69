1505f0544c98f5e5bf73a4f252abbafae41d1190
h1252 <h1252@MSI>
2025-05-09 20:04:25 +1000

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Team_69
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
